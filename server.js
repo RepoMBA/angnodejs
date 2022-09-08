1b80db8ecfb1586b6e5338811b90f4a35ce6b8cd
RepoMBA <63225534+RepoMBA@users.noreply.github.com>
2022-09-08 15:02:52 +0530

    https cert

--- server.js
+++ server.js
@@ -46,9 +46,20 @@ require("./routes/user.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+
+const https = require("https");
+const fs = require("fs");
+const https_options = {
+  ca: fs.readFileSync("ca_bundle.crt"),
+  key: fs.readFileSync("private.key"),
+  cert: fs.readFileSync("certificate.crt"),
+};
+https.createServer(https_options, app).listen(PORT);
+
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}.`);
+// });
 
 function initial() {
   Role.estimatedDocumentCount((err, count) => {
